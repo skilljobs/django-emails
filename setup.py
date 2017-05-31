54bb7bc6626e845df35793b702e7544dd5933d46
Frantisek Malina <frantisek.malina@interoute.com>
2017-05-31 13:33:06 +0100

    Add setup file to allow pip install.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import emails
+
+setup(
+    name="emails",
+    version=emails.__version__,
+    description='Mailing and processing for high volume senders and recipients.',
+    long_description=open('README.rst').read(),
+    license='BSD License',
+    platforms=['OS Independent'],
+    keywords='email,dkim,imap,subscriptions,bounces',
+    author='fmalina',
+    author_email='fmalina@gmail.com',
+    url="https://github.com/fmalina/emails",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open('requirements.txt').read().split(),
+)
