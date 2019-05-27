28e3fe9054275281134ebcb5e1f79fd9e99bfc95
Francis Malina <francis@vizualbod.com>
2019-05-27 11:06:59 +0100

    Backtrack

--- emails/management/commands/receiver.py
+++ emails/management/commands/receiver.py
@@ -4,5 +4,7 @@ from emails.imap_client import receive, IMAP4_SSL
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        receive(10 * 60)  # Cron will restart it every 10 mins.
-        return ''  # empty string to avoid output
+        try:
+            receive(10 * 60)  # Cron will restart it every 10 mins.
+        except IMAP4_SSL.abort:
+            return ''  # no output
