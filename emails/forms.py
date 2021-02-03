35d2aee383adecfa6d2ac1ade0ec62ef349bd4f0
F. Malina <fmalina@pm.me>
2021-02-03 06:54:56 +0100

    Made email category choices in email preferences form a callable to prevent migration errors

--- emails/forms.py
+++ emails/forms.py
@@ -2,10 +2,15 @@ from django import forms
 from emails.models import MailoutCategory
 
 
+def get_category_choices():
+    qs = MailoutCategory.objects.filter(default=True)\
+        .values_list('key', 'title')
+    return list(qs)
+
+
 class EmailPreferenceForm(forms.Form):
     choices = forms.MultipleChoiceField(
-        choices=list(MailoutCategory.objects.filter(default=True)\
-                                            .values_list('key', 'title')),
+        choices=get_category_choices,
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
