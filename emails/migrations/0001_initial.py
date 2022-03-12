8bed6b58f4319994ba87e71a3e1764e6d66b7358
F. Malina <fmalina@pm.me>
2022-03-12 03:03:09 +0000

    Fixed timezone in migration

--- emails/migrations/0001_initial.py
+++ emails/migrations/0001_initial.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import datetime
 from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -22,7 +21,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('subject', models.CharField(max_length=150)),
                 ('body', models.TextField()),
-                ('at', models.DateTimeField(default=datetime.datetime.now)),
+                ('at', models.DateTimeField(default=django.utils.timezone.now)),
                 ('bounced', models.BooleanField(default=False)),
                 ('to', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='emails', to=settings.AUTH_USER_MODEL)),
             ],
